5a97a3357fbd3e4c4f9091cce7910eb0cac6ca10
anton <anton@>
2026-03-07 13:04:33 +0300

    07.03.2026

---
+++ 2026.03.04-Homework-10/arraylist.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+class ArrayList{
+public:
+    int size = 0;
+    int capacity = 3;
+    double* data;
+
+    ArrayList(){data = new double[capacity];}   
+    ~ArrayList(){delete[] data;}
+
+    void add(double value){
+
+        if(size == capacity){
+            double* newdata = new double[capacity*2];
+            for(int i=0;i<capacity;i++){
+                data[i] = newdata[i];
+            }
+            delete[] data;
+            data = newdata;
+        }
+        data[size] = value;
+        size++;
+    }
+
+    void get(int index){
+        std::cout<<data[index]<<std::endl;
+    }
+    void set(int index, double value){
+        data[index] = value;
+    }
+    void remove(int index){
+        for(int i=index;i<size-1;i++){
+            data[i] = data[i+1];
+        }
+        size--;
+    }
+    void getsize(){
+        std::cout<<size<<std::endl;
+    }
+    void print(){
+        for(int i=0;i<size;i++){
+            std::cout<<data[i]<<std::endl;
+        }
+    }
+};
+
+
+int main(){
+    ArrayList list;
+
+    list.add(0.5);
+    list.add(1.5);
+    list.add(2.5);
+
+    list.get(2);
+
+    list.set(1,3.5);
+
+    list.remove(0);
+
+    list.getsize();
+    
+    list.print();
+
+    return 0;
+}
